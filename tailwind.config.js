142b001021f5513d0106db0613121f497ae05f5f
Kiriyako <92046315+Kiriyako@users.noreply.github.com>
2023-04-29 11:50:31 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./build/*.html'],
+  theme: {
+    extend: {
+      fontFamily:{
+        'sans': ['Century Gothic', 'sans-serif']
+      }
+    },
+  },
+  plugins: [],
+}
